f97abf9e13d44e6dbdc1cc90efa64074284bc9a1
mrintro <aniketpanwar.1998@gmail.com>
2022-02-21 13:20:15 +0530

    Let's get started commit.

---
+++ app/src/main/java/com/example/healtifiesme/navigation/NavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.healtifiesme.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.healtifiesme.R
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
